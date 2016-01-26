0102cddaafd288c58b1ae22f0da98eabae429ede
BestSean2016 <iwannabebest@sina.cn>
2016-01-26 22:12:11 +0800

    facctoring

--- newton-frac.m
+++
@@ -1,6 +0,0 @@
-%newton's method to slove sqrt(b)
-b=38;
-x=2;
-for k = 1 : 10
-    x = (2 - b * x) * x
-end
--- newton.m
+++ newton.m
@@ -4,3 +4,12 @@ x=2;
 for k = 1 : 10
     x = (x + b / x) * 0.5
 end
+
+
+%newton's method to slove 1/b
+b=38;
+x=0.01;
+for k = 1 : 10
+    x = (2 - b * x) * x
+end
+
